f3b827725a752d1004aedf4ab77fee9d0d11ddf3
Madhu L <155795791+MadhuL56@users.noreply.github.com>
2025-04-21 09:10:04 +0530

    first commit

--- Local_food_reduction 6 (2)/Local_food_reduction 6/Local_food_reduction/app/routes/receiver_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import login_required, current_user
-from ..models import db, FoodItem
-
-
-::contentReference[oaicite:18]{index=18}
- 
